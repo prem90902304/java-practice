e1d2f67d9024a2d6e2b90bbf9223546d12a11d21
MUPPALAM PREMSAI <premsaim23@gmail.com>
2026-03-13 10:49:03 +0530

    Add files via upload

---
+++ pattern/a2.java
@@ -0,0 +1,38 @@
+public class a2 {
+    public static void main(String[] args) {
+        int n=5;
+        for(int i =1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            for(int j=i;j<n;j++){
+                System.out.print("1 ");
+            }
+            for(int j=i;j<=n;j++){
+                System.out.print("1 ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        
+        }
+        for(int i =1;i<=n;i++){
+            for(int j=i;j<n;j++){
+                System.out.print("* ");
+            }
+            for(int j=i;j<=n;j++){
+                System.out.print("  ");
+            }
+            for(int j=1;j<i;j++){
+                System.out.print("* ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+
+    }
+    
+}
+}
---
+++ pattern/array.java
@@ -0,0 +1,6 @@
+public class array {
+    public static void main(String[] args) {
+        int array[1]={10};
+        System.out.println(array[1]);
+    }
+}
